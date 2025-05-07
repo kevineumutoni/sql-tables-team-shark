6c902429ffb4f3a452c02eea57f8e9206ead8ef5
Student <shirleyabwoi@gmail.com>
2025-05-07 19:02:20 +0300

    Added the parent table

--- Script.sql
+++ Script.sql
@@ -29,3 +29,31 @@ VALUES('T01','CT01','2025-04-10',080.23,100.67,4);
 ('T14','CT14','2025-04-25',85.61,110.84,3),
 ('T15','CT15','2025-04-27',95.78,101.41,4);
 
+create table malnutrition.parent_table(
+parent_id VARCHAR (5) primary key,
+parent_name VARCHAR (100),
+phone_number VARCHAR (100),
+national_id VARCHAR (20)
+);
+
+insert into malnutrition.parent_table (parent_id, parent_name,phone_number, national_id)
+values
+('PRO1', 'Maina', '0708539079', '123456'),
+('PR02', 'Mbugua', '0706536876', '65432'),
+('PR03', 'Shirley', '0768943674', '846724'),
+('PR04', 'Bilha', '076543218', '7536281'),
+('PR05', 'Umutoni', '07532763452', '8643234'),
+('PR06', 'Mutua', '075377234', '3487234'),
+('PR07', 'Kevine', '075236456', '2342343'),
+('PR08', 'Semhal', '076452387', '36432784'),
+('PR09', 'Estifanos', '0764523643', '6576437'),
+('PR10', 'Judy', '074366237', '4753872'),
+('PR11', 'Gikuni', '072354234', '7328397'),
+('PR12', 'Kamau', '0746374648', '73246234'),
+('PR13', 'Hannah', '07526374', '6327474'),
+('PR14', 'Saloi', '075774234', '7539324'),
+('PR15', 'Jacky', '8573645', '76354235');
+
+select * from parent_table
+
+where name like 'K%'or 'k%';
